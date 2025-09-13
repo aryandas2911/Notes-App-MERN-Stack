e7175d9aaa59a99e07dd90611a933d623822f215
Aryan Das <aryandas2911@gmail.com>
2025-09-13 23:03:48 +0530

    Signup page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Signup from './pages/signup'
 
 const App = () => {
   return (
     <div>
-      
-      </div>
+      <Signup/>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const reponse = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        { name, email, password }
+      );
+      
+    } catch (error) {}
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="border border-gray-200 shadow p-6 w-80 bg-white">
+        <h2 className="text-2xl font-bold mb-4">Signup</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-700">
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter Username"
+              required
+              className="w-full px-3 py-2 border border-gray-300"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700">
+              Email
+            </label>
+            <input
+              id="email"
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email"
+              required
+              className="w-full px-3 py-2 border border-gray-300"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="*****"
+              required
+              className="w-full px-3 py-2 border border-gray-300"
+            />
+          </div>
+          <button type="submit" className="w-full bg-teal-600 text-white py-2">
+            Signup
+          </button>
+          <p className="text-center mt-2">
+            Already Have an Account? <a href="">Login</a>{" "}
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
